26087f15fda6fa5a45fa60b07b508b859f1b93e5
Aaron Grincewicz <asgrincewicz@icloud.com>
2021-11-04 19:13:25 -0400

    Implemented Jumping.

--- main.cpp
+++ main.cpp
@@ -3,15 +3,26 @@
 int main()
 {
    //Window Dimensions
-   int windowWidth = 800;
-   int windowHeight = 600;
+   const int windowWidth = 800;
+   const int windowHeight = 600;
    InitWindow(windowWidth, windowHeight, "Dapper Dasher");
-
-    while(WindowShouldClose()== false)
+   //rectangle dimensions
+   const int rectWidth = 80;
+   const int rectHeight = 50;
+   int posY = windowHeight - rectHeight;
+   int velocity = 0;
+    SetTargetFPS(60);
+    while(!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
+        posY += velocity;
+        DrawRectangle(windowWidth/2, posY, rectWidth, rectHeight, RED);
+        if(IsKeyPressed(KEY_SPACE))
+        {
+            velocity -= 10;
+        }
         EndDrawing();
     }
-
+    CloseWindow();
 }
